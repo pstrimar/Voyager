f4370f08a86132cd5a4c8bcbdbbe11a446ed56b4
Pierson Trimarchi <pstrimar@gmail.com>
2023-06-26 14:05:42 -0400

    premake

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "Voyager"
+	architecture "x64"
+	startproject "Sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Voyager"
+	location "Voyager"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"VGR_PLATFORM_WINDOWS",
+			"VGR_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{			
+			 ("{COPY} %{cfg.buildtarget.relpath} \"../bin/" .. outputdir .. "/Sandbox/\"")
+		}
+
+	filter "configurations:Debug"
+		defines "VGR_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "VGR_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "VGR_DIST"
+		optimize "On"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"Voyager/vendor/spdlog/include",
+		"Voyager/src"
+	}
+
+	links
+	{
+		"Voyager"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"VGR_PLATFORM_WINDOWS"
+		}
+
+	filter "configurations:Debug"
+		defines "VGR_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "VGR_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "VGR_DIST"
+		optimize "On"
