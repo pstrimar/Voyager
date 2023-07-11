8480892745a818df88592c96b48e117aeb36c9be
Pierson Trimarchi <pstrimar@gmail.com>
2023-07-11 18:03:45 -0400

    openGL shader abstraction and the beginnings of a material system

--- Sandbox/src/SandboxApp.cpp
+++ Sandbox/src/SandboxApp.cpp
@@ -1,8 +1,11 @@
 #include <Voyager.h>
 
+#include "Voyager/Platform/OpenGL/OpenGLShader.h"
+
 #include "imgui/imgui.h"
 
 #include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
 
 class ExampleLayer : public Voyager::Layer
 {
@@ -92,9 +95,9 @@ public:
 			}			
 		)";
 
-		m_Shader.reset(new Voyager::Shader(vertexSrc, fragmentSrc));
+		m_Shader.reset(Voyager::Shader::Create(vertexSrc, fragmentSrc));
 
-		std::string blueVertexSrc = R"(
+		std::string flatColorShaderVertexSrc = R"(
 			#version 330 core
 
 			layout(location = 0) in vec3 a_Position;
@@ -111,20 +114,22 @@ public:
 			}			
 		)";
 
-		std::string blueFragmentSrc = R"(
+		std::string flatColorShaderFragmentSrc = R"(
 			#version 330 core
 
 			layout(location = 0) out vec4 color;
 
 			in vec3 v_Position;
+
+			uniform vec3 u_Color;
 	
 			void main()
 			{
-				color = vec4(0.2, 0.3, 0.8, 1.0);
+				color = vec4(u_Color, 1.0);
 			}			
 		)";
 
-		m_BlueShader.reset(new Voyager::Shader(blueVertexSrc, blueFragmentSrc));
+		m_FlatColorShader.reset(Voyager::Shader::Create(flatColorShaderVertexSrc, flatColorShaderFragmentSrc));
 	}
 
 	void OnUpdate(Voyager::Timestep ts) override
@@ -157,13 +162,16 @@ public:
 
 		glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
 
+		std::dynamic_pointer_cast<Voyager::OpenGLShader>(m_FlatColorShader)->Bind();
+		std::dynamic_pointer_cast<Voyager::OpenGLShader>(m_FlatColorShader)->UploadUniformFloat3("u_Color", m_SquareColor);
+
 		for (int y = 0; y < 20; y++)
 		{
 			for (int x = 0; x < 20; x++)
 			{
 				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
 				glm::mat4 transform = glm::translate(glm::mat4(1.0f), pos) * scale;
-				Voyager::Renderer::Submit(m_BlueShader, m_SquareVA, transform);
+				Voyager::Renderer::Submit(m_FlatColorShader, m_SquareVA, transform);
 			}
 		}
 
@@ -174,7 +182,9 @@ public:
 
 	virtual void OnImGuiRender() override
 	{
-		
+		ImGui::Begin("Settings");
+		ImGui::ColorEdit3("Square Color", glm::value_ptr(m_SquareColor));
+		ImGui::End();
 	}
 
 	void OnEvent(Voyager::Event& event) override
@@ -183,7 +193,7 @@ public:
 	}
 private:
 	std::shared_ptr<Voyager::Shader> m_Shader;
-	std::shared_ptr<Voyager::Shader> m_BlueShader;
+	std::shared_ptr<Voyager::Shader> m_FlatColorShader;
 	std::shared_ptr<Voyager::VertexArray> m_VertexArray;
 
 	std::shared_ptr<Voyager::VertexArray> m_SquareVA;
@@ -193,6 +203,8 @@ private:
 	float m_CameraRotation = 0.0f;
 	float m_CameraMoveSpeed = 5.0f;
 	float m_CameraRotationSpeed = 180.0f;
+
+	glm::vec3 m_SquareColor = { 0.2f, 0.3f, 0.8f };
 };
 
 class Sandbox : public Voyager::Application
