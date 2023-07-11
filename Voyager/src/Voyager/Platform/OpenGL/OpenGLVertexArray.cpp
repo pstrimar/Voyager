07f0eeb5cb5db1817c8a2eb7b063fe76274aa9fa
Pierson Trimarchi <pstrimar@gmail.com>
2023-07-10 21:25:29 -0400

    vertex array class abstraction

--- Voyager/src/Voyager/Platform/OpenGL/OpenGLVertexArray.cpp
+++ Voyager/src/Voyager/Platform/OpenGL/OpenGLVertexArray.cpp
@@ -5,10 +5,29 @@
 
 namespace Voyager {
 
+	static GLenum ShaderDataTypeToOpenGLBaseType(ShaderDataType type)
+	{
+		switch (type)
+		{
+		case ShaderDataType::None: return GL_FLOAT;
+		case ShaderDataType::Float2: return GL_FLOAT;
+		case ShaderDataType::Float3: return GL_FLOAT;
+		case ShaderDataType::Float4: return GL_FLOAT;
+		case ShaderDataType::Mat3: return GL_FLOAT;
+		case ShaderDataType::Mat4: return GL_FLOAT;
+		case ShaderDataType::Int: return GL_INT;
+		case ShaderDataType::Int2: return GL_INT;
+		case ShaderDataType::Int3: return GL_INT;
+		case ShaderDataType::Int4: return GL_INT;
+		case ShaderDataType::Bool: return GL_BOOL;
+		}
+		VGR_CORE_ASSERT(false, "Unknown ShaderDataType!");
+		return 0;
+	}
+
 	OpenGLVertexArray::OpenGLVertexArray()
 	{
 		glCreateVertexArrays(1, &m_RendererID);
-		glBindVertexArray(m_RendererID);
 	}
 
 	OpenGLVertexArray::~OpenGLVertexArray()
@@ -24,6 +43,40 @@ namespace Voyager {
 	void Voyager::OpenGLVertexArray::Unbind() const
 	{
 		glBindVertexArray(0);
+	}
+
+	void OpenGLVertexArray::AddVertexBuffer(const std::shared_ptr<VertexBuffer>& vertexBuffer)
+	{
+		glBindVertexArray(m_RendererID);
+		vertexBuffer->Bind();
+
+		VGR_CORE_ASSERT(vertexBuffer->GetLayout().GetElements().size(), "Vertex buffer has no layout!");
+
+		uint32_t index = 0;
+		const auto& layout = vertexBuffer->GetLayout();
+		for (const auto& element : layout)
+		{
+			glEnableVertexAttribArray(index);
+			glVertexAttribPointer(
+				index,
+				element.GetComponentCount(),
+				ShaderDataTypeToOpenGLBaseType(element.Type),
+				element.Normalized ? GL_TRUE : GL_FALSE,
+				layout.GetStride(),
+				(void*)element.Offset
+			);
+
+			index++;
+		}
+		m_VertexBuffers.push_back(vertexBuffer);
+	}
+
+	void OpenGLVertexArray::SetIndexBuffer(const std::shared_ptr<IndexBuffer>& indexBuffer)
+	{
+		glBindVertexArray(m_RendererID);
+		indexBuffer->Bind();
+
+		m_IndexBuffer = indexBuffer;
 	}
 
 }
--- Voyager/src/Voyager/Renderer/Buffer.h
+++ Voyager/src/Voyager/Renderer/Buffer.h
@@ -1,8 +1,5 @@
 #pragma once
 
-#include <vector>
-#include <string>
-
 namespace Voyager {
 
 	enum class ShaderDataType
