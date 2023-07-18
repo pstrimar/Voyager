8527c143c28bb14773e540f9e8db3b25feb6ce7e
Pierson Trimarchi <pstrimar@gmail.com>
2023-07-18 17:33:44 -0400

    adding tinting and rotating support for drawing quads

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -40,9 +40,9 @@ void Sandbox2D::OnUpdate(Voyager::Timestep ts)
 	{
 		VGR_PROFILE_SCOPE("Renderer Draw");
 		Voyager::Renderer2D::BeginScene(m_CameraController.GetCamera());
-		Voyager::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
-		Voyager::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f }, { 0.2f, 0.3f, 0.8f, 1.0f });
-		Voyager::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture);
+		Voyager::Renderer2D::DrawRotatedQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(-45.0f), { 0.8f, 0.2f, 0.3f, 1.0f });
+		Voyager::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f } ,{ 0.2f, 0.3f, 0.8f, 1.0f });
+		Voyager::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
 		Voyager::Renderer2D::EndScene();
 	}
 }
--- Voyager/src/Voyager/Platform/OpenGL/OpenGLShader.cpp
+++ Voyager/src/Voyager/Platform/OpenGL/OpenGLShader.cpp
@@ -205,6 +205,13 @@ namespace Voyager {
 		UploadUniformInt(name, value);
 	}
 
+	void OpenGLShader::SetFloat(const std::string& name, float value)
+	{
+		VGR_PROFILE_FUNCTION();
+
+		UploadUniformFloat(name, value);
+	}
+
 	void OpenGLShader::SetFloat3(const std::string& name, const glm::vec3& value)
 	{
 		VGR_PROFILE_FUNCTION();
