2ee24fb0967a804bbafdec532e78bf510398ef40
Pierson Trimarchi <pstrimar@gmail.com>
2023-07-18 12:20:42 -0400

    combined color and texture shader into one
    Only have to bind the shader in begin scene now

--- Voyager/src/Voyager/Renderer/Texture.cpp
+++ Voyager/src/Voyager/Renderer/Texture.cpp
@@ -5,6 +5,20 @@
 
 namespace Voyager {
 
+	Ref<Texture2D> Texture2D::Create(uint32_t width, uint32_t height)
+	{
+		switch (Renderer::GetAPI())
+		{
+		case RendererAPI::API::None:
+			VGR_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
+			return nullptr;
+		case RendererAPI::API::OpenGL:
+			return CreateRef<OpenGLTexture2D>(width, height);
+		}
+		VGR_CORE_ASSERT(false, "Unknown RendererAPI!");
+		return nullptr;
+	}
+
 	Ref<Texture2D> Texture2D::Create(const std::string& path)
 	{
 		switch (Renderer::GetAPI())
@@ -13,7 +27,7 @@ namespace Voyager {
 			VGR_CORE_ASSERT(false, "RendererAPI::None is currently not supported!");
 			return nullptr;
 		case RendererAPI::API::OpenGL:
-			return std::make_shared<OpenGLTexture2D>(path);
+			return CreateRef<OpenGLTexture2D>(path);
 		}
 		VGR_CORE_ASSERT(false, "Unknown RendererAPI!");
 		return nullptr;
