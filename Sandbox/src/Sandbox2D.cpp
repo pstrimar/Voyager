c68746d75be158954ed220686a9854f4c8fd9922
Pierson Trimarchi <pstrimar@gmail.com>
2023-07-19 12:43:21 -0400

    Renderer stats and batch improvements

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -31,6 +31,7 @@ void Sandbox2D::OnUpdate(Voyager::Timestep ts)
 	m_CameraController.OnUpdate(ts);
 
 	// Render
+	Voyager::Renderer2D::ResetStats();
 	{
 		VGR_PROFILE_SCOPE("Renderer Prep");
 		Voyager::RenderCommand::SetClearColor({ 0.1f, 0.1f, 0.1f, 1 });
@@ -46,8 +47,17 @@ void Sandbox2D::OnUpdate(Voyager::Timestep ts)
 		Voyager::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, -45.0f, { 0.8f, 0.2f, 0.3f, 1.0f });
 		Voyager::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 		Voyager::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f } ,{ 0.2f, 0.3f, 0.8f, 1.0f });
-		Voyager::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 10.0f, 10.0f }, m_CheckerboardTexture, 10.0f);
+		Voyager::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 20.0f, 20.0f }, m_CheckerboardTexture, 10.0f);
 		Voyager::Renderer2D::DrawRotatedQuad({ 0.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, rotation, m_CheckerboardTexture, 20.0f);
+
+		for (float y = -5.0f; y < 5.0f; y += 0.5f)
+		{
+			for (float x = -5.0f; x < 5.0f; x += 0.5f)
+			{
+				glm::vec4 color = { (x + 5.0f) / 10.0f, 0.4f, (y + 5.0f) / 10.0f, 0.7f };
+				Voyager::Renderer2D::DrawQuad({ x, y }, { 0.45f, 0.45f }, color);
+			}
+		}
 		Voyager::Renderer2D::EndScene();
 	}
 }
@@ -57,6 +67,14 @@ void Sandbox2D::OnImGuiRender()
 	VGR_PROFILE_FUNCTION();
 
 	ImGui::Begin("Settings");
+
+	auto stats = Voyager::Renderer2D::GetStats();
+	ImGui::Text("Renderer2D Stats:");
+	ImGui::Text("Draw Calls: %d", stats.DrawCalls);
+	ImGui::Text("Quads: %d", stats.QuadCount);
+	ImGui::Text("Vertices: %d", stats.GetTotalVertexCount());
+	ImGui::Text("Indices: %d", stats.GetTotalIndexCount());
+
 	ImGui::ColorEdit4("Square Color", glm::value_ptr(m_SquareColor));
 	ImGui::End();
 }
