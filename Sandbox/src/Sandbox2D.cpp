913bb3a72ff1278bdf6a01eeed39231de8e0935a
Pierson Trimarchi <pstrimar@gmail.com>
2023-07-19 14:50:15 -0400

    testing out a particle system with lots of quads

--- Sandbox/src/Sandbox2D.cpp
+++ Sandbox/src/Sandbox2D.cpp
@@ -16,6 +16,15 @@ void Sandbox2D::OnAttach()
 	VGR_PROFILE_FUNCTION();
 
 	m_CheckerboardTexture = Voyager::Texture2D::Create("assets/textures/Checkerboard.png");
+
+	// Init here
+	m_Particle.ColorBegin = { 254 / 255.0f, 212 / 255.0f, 123 / 255.0f, 1.0f };
+	m_Particle.ColorEnd = { 254 / 255.0f, 109 / 255.0f, 41 / 255.0f, 1.0f };
+	m_Particle.SizeBegin = 0.5f, m_Particle.SizeVariation = 0.3f, m_Particle.SizeEnd = 0.0f;
+	m_Particle.LifeTime = 5.0f;
+	m_Particle.Velocity = { 0.0f, 0.0f };
+	m_Particle.VelocityVariation = { 3.0f, 1.0f };
+	m_Particle.Position = { 0.0f, 0.0f };
 }
 
 void Sandbox2D::OnDetach()
@@ -44,11 +53,11 @@ void Sandbox2D::OnUpdate(Voyager::Timestep ts)
 
 		VGR_PROFILE_SCOPE("Renderer Draw");
 		Voyager::Renderer2D::BeginScene(m_CameraController.GetCamera());
-		Voyager::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, -45.0f, { 0.8f, 0.2f, 0.3f, 1.0f });
+		Voyager::Renderer2D::DrawRotatedQuad({ 1.0f, 0.0f }, { 0.8f, 0.8f }, glm::radians(- 45.0f), {0.8f, 0.2f, 0.3f, 1.0f});
 		Voyager::Renderer2D::DrawQuad({ -1.0f, 0.0f }, { 0.8f, 0.8f }, { 0.8f, 0.2f, 0.3f, 1.0f });
 		Voyager::Renderer2D::DrawQuad({ 0.5f, -0.5f }, { 0.5f, 0.75f } ,{ 0.2f, 0.3f, 0.8f, 1.0f });
 		Voyager::Renderer2D::DrawQuad({ 0.0f, 0.0f, -0.1f }, { 20.0f, 20.0f }, m_CheckerboardTexture, 10.0f);
-		Voyager::Renderer2D::DrawRotatedQuad({ 0.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, rotation, m_CheckerboardTexture, 20.0f);
+		Voyager::Renderer2D::DrawRotatedQuad({ -2.0f, 0.0f, 0.0f }, { 1.0f, 1.0f }, glm::radians(rotation), m_CheckerboardTexture, 20.0f);
 
 		for (float y = -5.0f; y < 5.0f; y += 0.5f)
 		{
@@ -60,6 +69,24 @@ void Sandbox2D::OnUpdate(Voyager::Timestep ts)
 		}
 		Voyager::Renderer2D::EndScene();
 	}
+
+	if (Voyager::Input::IsMouseButtonPressed(VGR_MOUSE_BUTTON_LEFT))
+	{
+		auto [x, y] = Voyager::Input::GetMousePosition();
+		auto width = Voyager::Application::Get().GetWindow().GetWidth();
+		auto height = Voyager::Application::Get().GetWindow().GetHeight();
+
+		auto bounds = m_CameraController.GetBounds();
+		auto pos = m_CameraController.GetCamera().GetPosition();
+		x = (x / width) * bounds.GetWidth() - bounds.GetWidth() * 0.5f;
+		y = bounds.GetHeight() * 0.5f - (y / height) * bounds.GetHeight();
+		m_Particle.Position = { x + pos.x, y + pos.y };
+		for (int i = 0; i < 50; i++)
+			m_ParticleSystem.Emit(m_Particle);
+	}
+
+	m_ParticleSystem.OnUpdate(ts);
+	m_ParticleSystem.OnRender(m_CameraController.GetCamera());
 }
 
 void Sandbox2D::OnImGuiRender()
--- Sandbox/src/Sandbox2D.h
+++ Sandbox/src/Sandbox2D.h
@@ -2,6 +2,8 @@
 
 #include "Voyager.h"
 
+#include "ParticleSystem.h"
+
 class Sandbox2D : public Voyager::Layer
 {
 public:
@@ -24,4 +26,7 @@ private:
 	Voyager::Ref<Voyager::Texture2D> m_CheckerboardTexture;
 	
 	glm::vec4 m_SquareColor = { 0.2f, 0.3f, 0.8f, 1.0f };
+
+	ParticleProps m_Particle;
+	ParticleSystem m_ParticleSystem;
 };
