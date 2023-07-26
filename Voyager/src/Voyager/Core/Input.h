70cc0f21029037dec145a79aa23688a042930a3b
Pierson Trimarchi <pstrimar@gmail.com>
2023-07-26 12:18:28 -0400

    refactored input

--- Voyager/src/Voyager/Core/Input.h
+++ Voyager/src/Voyager/Core/Input.h
@@ -4,30 +4,15 @@
 
 namespace Voyager {
 
-	class VOYAGER_API Input
+	class Input
 	{
 	public:
-		virtual ~Input() = default;
-		Input(const Input&) = delete;
-		Input& operator=(const Input&) = delete;
+		static bool IsKeyPressed(int keycode);
 
-		inline static bool IsKeyPressed(int keycode) { return s_Instance->IsKeyPressedImpl(keycode); }
-
-		inline static bool IsMouseButtonPressed(int button) { return s_Instance->IsMouseButtonPressedImpl(button); }
-		inline static std::pair<float, float> GetMousePosition() { return s_Instance->GetMousePositionImpl(); }
-		inline static float GetMouseX() { return s_Instance->GetMouseXImpl(); }
-		inline static float GetMouseY() { return s_Instance->GetMouseYImpl(); }
-
-
-	protected:
-		virtual bool IsKeyPressedImpl(int keycode) = 0;
-
-		virtual bool IsMouseButtonPressedImpl(int button) = 0;
-		virtual std::pair<float, float> GetMousePositionImpl() = 0;
-		virtual float GetMouseXImpl() = 0;
-		virtual float GetMouseYImpl() = 0;
-	private:
-		static Input* s_Instance;
+		static bool IsMouseButtonPressed(int button);
+		static std::pair<float, float> GetMousePosition();
+		static float GetMouseX();
+		static float GetMouseY();
 	};
 
 }
