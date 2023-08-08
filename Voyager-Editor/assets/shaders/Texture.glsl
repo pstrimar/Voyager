e1c87d0e8e5fa0d97807cc2e7d20cc2433d25ba2
Pierson Trimarchi <pstrimar@gmail.com>
2023-08-08 12:57:16 -0400

    Fixed TexIndex being interpolated in Texture shader

--- Voyager-Editor/assets/shaders/Texture.glsl
+++ Voyager-Editor/assets/shaders/Texture.glsl
@@ -30,7 +30,6 @@ void main()
 {
 	Output.Color = a_Color;
 	Output.TexCoord = a_TexCoord;
-	Output.TexIndex = a_TexIndex;
 	Output.TilingFactor = a_TilingFactor;
 	v_TexIndex = a_TexIndex;
 	v_EntityID = a_EntityID;
@@ -48,11 +47,11 @@ struct VertexOutput
 {
 	vec4 Color;
 	vec2 TexCoord;
-	float TexIndex;
 	float TilingFactor;
 };
 
 layout (location = 0) in VertexOutput Input;
+layout (location = 3) in flat float v_TexIndex;
 layout (location = 4) in flat int v_EntityID;
 
 layout (binding = 0) uniform sampler2D u_Textures[32];
